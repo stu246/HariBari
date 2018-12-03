3e9460390f5c73448d81024d932c640cc9a35378
OrelKaravani <Orel1192@gmail.com>
2018-12-03 13:41:48 +0200

    Some function headers and todos

--- RequestsServer/index.js
+++ RequestsServer/index.js
@@ -8,7 +8,29 @@ app.get('/keepalive', function (req, res) {
 });
 
 /// TODO: POST receive file from client.
+app.post('/missionOutput', function(req, res){
+
+})
 
 /// TODO: RESTAPI Tasks
+
+// Kill agent
+app.get('/killAgent/:agentId', function(req, res){
+  res.send("Agent id is "+ req.params.agentId)
+})
+
+// DB queries
+// Find all the missions in the DB
+app.get('/getMissions', function(req, res){
+
+})
+
+//Find all the agents
+app.get('/getAllAgents', function (req, res){
+
+})
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
